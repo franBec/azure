52f66c9227329bc98127c67532d7bb92962bd53a
franBec <franbecvort@gmail.com>
2025-05-13 21:31:03 +0100

    Update homepage button link and text
    Changed the button link from "/docs/intro" to "/intro" and updated its text to "Let's start! ☁️" for a more engaging call-to-action.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/intro">
+            Let's start! ☁️
           </Link>
         </div>
       </div>
